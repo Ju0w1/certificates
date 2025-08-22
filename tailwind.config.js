9d6da4f46830288cd98bd4f7e797a65b81a7b9a7
Juan Pablo Peculio <peculio.pablo@gmail.com>
2025-08-22 18:35:52 -0300

    Crear proyecto de certificados

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    darkMode: 'media', // Usa el modo del sistema automáticamente
+    content: [
+      "./src/**/*.{astro,js,ts,jsx,tsx,md}",
+      "./public/**/*.html"
+    ],
+    theme: {
+      extend: {}
+    },
+    plugins: []
+  }
